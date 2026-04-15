0a678ce2e6058915165713fb0eaa3737658c9181
KennyB-hub <kennybennett11477@gmail.com>
2026-04-15 07:10:03 -0400

    Create index.js (#191)

---
+++ src/ai/tools/gov/index.js
@@ -0,0 +1,9 @@
+import { generateRecommendations } from "./generateRecommendations.js";
+import { sectorAnalysis } from "./sectorAnalysis.js";
+import { formatAdvisory } from "./formatAdvisory.js";
+
+export default {
+    generateRecommendations,
+    sectorAnalysis,
+    formatAdvisory
+};
