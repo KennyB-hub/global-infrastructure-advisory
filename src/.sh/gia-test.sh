e4d022bdec6a4cff0109820e37fc75f07afb0634
KennyB-hub <kennybennett11477@gmail.com>
2026-04-15 23:37:58 -0400

    Update gia-test.sh

--- src/.sh/gia-test.sh
+++ src/.sh/gia-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -X POST "https://YOUR-WORKER-URL.workers.dev" \
+curl -X POST "https://global-infrastructure-advisory.global-infrastructure-advisory.workers.dev.workers.dev" \
   -H "Content-Type: application/json" \
   -d '{
         "workflow": "publicBriefing",
