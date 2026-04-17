51b61d7dd5aebe025f9917ea70208e2a600d8c71
KennyB-hub <kennybennett11477@gmail.com>
2026-04-17 01:26:28 -0400

    Update helpers.js (#225)

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,8 @@
+export const formatLog = (data) => {
+    return typeof data === 'string' ? data : JSON.stringify(data, null, 2);
+};
+
+export const generateResonanceKey = (query) => {
+    return `resonance:${query.trim().toLowerCase().replace(/\s+/g, '_')}`;
+};
 
