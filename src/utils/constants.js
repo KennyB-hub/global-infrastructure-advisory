6afe50254d369f76cd92a220baaf50ebed1d9605
KennyB-hub <kennybennett11477@gmail.com>
2026-04-17 01:24:16 -0400

    Update constants.js (#224)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,11 @@
+export const GIA_VERSION = "12.0.0";
+export const TRUST_ZONES = {
+    PUBLIC: "public",
+    ADMIN: "admin",
+    INTERNAL: "internal"
+};
+export const SYSTEM_MODELS = {
+    PRIMARY: "@cf/meta/llama-3-8b-instruct",
+    FAST: "@cf/meta/llama-2-7b-chat-int8"
+};
 
