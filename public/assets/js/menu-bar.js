ee9c49a3f6afe1178d495f147403c7e43dfb56f6
KennyB-hub <kennybennett11477@gmail.com>
2026-04-19 17:22:32 -0400

    Create menu-bar.js

---
+++ public/assets/js/menu-bar.js
@@ -0,0 +1,9 @@
+const menuItems = [
+  { title: 'Home', url: '/' },
+  { title: 'Capabilities', url: '/capabilities' },
+  { title: 'Services', url: '/services' },
+  { title: 'Case Studies', url: '/case-studies' },
+  { title: 'Technical Operations', url: '/technical-operations' },
+  { title: 'Contact', url: '/contact' }
+];
+export default menuItems;
