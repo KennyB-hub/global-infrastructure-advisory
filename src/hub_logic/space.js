094a1bcb63dcebe408fbe456d4ac228c74218991
KennyB-hub <kennybennett11477@gmail.com>
2026-05-23 20:19:53 +0000

    git commit merge updates

--- src/hub_logic/space.js
+++
@@ -1,6 +0,0 @@
-{
-  "id": "space",
-  "title": "Orbital Infrastructure",
-  "description": "Supporting satellite arrays and lunar base power grids...",
-  "features": ["Orbital Solar", "Lunar WASH systems"]
-}
