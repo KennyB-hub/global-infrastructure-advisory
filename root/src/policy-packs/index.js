503d4b4adb517a71ec647e07fe13abb6608106f6
Kenneth Bennett <kennybennett11477@gmail.com>
2026-05-28 18:24:30 -0400

    git commit

--- root/src/policy-packs/index.js
+++ root/src/policy-packs/index.js
@@ -1,10 +1,10 @@
-import commonRule from "../sovereign/common-rule.json" assert { type: "json" };
-import dhsCompliance from "../sovereign/dhs-compliance.json" assert { type: "json" };
-import ncesEthics from "../sovereign/nces-ethics.json" assert { type: "json" };
-import ogeMisconduct from "../sovereign/oge-misconduct.json" assert { type: "json" };
-import ombM1406 from "../sovereign/omb-m14-06.json" assert { type: "json" };
-import ombM2012 from "../sovereign/omb-m20-12.json" assert { type: "json" };
-import ostpIntegrity from "../sovereign/ostp-scientific-integrity.json" assert { type: "json" };
+import commonRule from "./sovereign/common-rule.json" assert { type: "json" };
+import dhsCompliance from "./sovereign/dhs-compliance.json" assert { type: "json" };
+import ncesEthics from "./sovereign/nces-ethics.json" assert { type: "json" };
+import ogeMisconduct from "./sovereign/oge-misconduct.json" assert { type: "json" };
+import ombM1406 from "./sovereign/omb-m14-06.json" assert { type: "json" };
+import ombM2012 from "./sovereign/omb-m20-12.json" assert { type: "json" };
+import ostpIntegrity from "./sovereign/ostp-scientific-integrity.json" assert { type: "json" };
 
 export const SovereignPolicyPacks = {
   "common-rule": commonRule,
