6ce334b356affccc7274fb7c655bf47467db3933
KennyB-hub <kenny@example.com>
2026-06-27 15:53:55 -0400

    git commit

--- proprietary-cli/commands/dev/clean.js
+++ proprietary-cli/commands/dev/clean.js
@@ -1,3 +1,10 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath, pathToFileURL } from "url";
+import { readJSON } from "../helpers/json.js";
+import { resolvePath } from "../helpers/paths.js";
+import { buildContext } from "../context/context.js";
+
 export const name = "clean"
 
 export function run() {
