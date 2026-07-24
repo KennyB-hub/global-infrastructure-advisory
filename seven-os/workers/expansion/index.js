b5666c53b41c01b64707267bd4f1b4dcb3bb4191
KennyB-hub <kenny@example.com>
2026-07-24 17:28:48 -0400

    git commit

--- seven-os/workers/expansion/index.js
+++ seven-os/workers/expansion/index.js
@@ -1,7 +1,1 @@
-// --- SEVEN-OS AUTOMATED LEDGER TRACKING HOOK ---
-import { SevenOsLedgerManager } from "../../utils/ledger-manager";
-const _ledger = new SevenOsLedgerManager();
-_ledger.logWorkerEvidence("expansion", "online", "Autonomous worker runtime initialization cycle verified.");
-// -----------------------------------------------
 module.exports = {};
-
